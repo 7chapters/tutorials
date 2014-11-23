97fd908cd334031f8b6fbff5a833fdb45856f8c1
Jayram Rout <jayram.rout@7chapters.info>
2014-11-23 16:57:34 -0500

    update

--- WebService_First/src/main/java/com/tutorial/ws/helloworld/HelloWorld.java
+++ WebService_First/src/main/java/com/tutorial/ws/helloworld/HelloWorld.java
@@ -1,6 +1,9 @@
 package com.tutorial.ws.helloworld;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebParam.Mode;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.ParameterStyle;
@@ -8,11 +11,14 @@ import javax.jws.soap.SOAPBinding.Style;
 import javax.jws.soap.SOAPBinding.Use;
 
 /**
- * @author Jayram
- * First Create a Web Service Endpoint Interface
+ * @author Jayram First Create a Web Service Endpoint Interface
  */
-@WebService
-@SOAPBinding(style = Style.DOCUMENT, parameterStyle = ParameterStyle.WRAPPED , use= Use.LITERAL)
+@WebService(name = "HelloWorldAsService", targetNamespace = "http://7chapters.com")
+@SOAPBinding(style = Style.DOCUMENT, parameterStyle = ParameterStyle.WRAPPED, use = Use.ENCODED)
 public interface HelloWorld {
-	@WebMethod String getHelloWorld(String name, String nametwo);
+	@WebMethod(operationName = "getHelloWorldRequest")
+	@WebResult(name = "HelloWorldResponse", targetNamespace = "http://7chapters.com/ws/types")
+	String getHelloWorld(
+			@WebParam(targetNamespace = "http://7chapters.com/ws/types", name = "argOne", mode = Mode.IN) String name,
+			@WebParam(targetNamespace = "http://7chapters.com/ws/types", name = "argTwo", mode = Mode.IN) String nametwo);
 }
